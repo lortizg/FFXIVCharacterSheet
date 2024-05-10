e4afdd7325657e25262c41cad8f904a3d26c96e3
lortizg <laura.og@gmail.com>
2024-05-10 20:37:10 +0200

    Added reference

---
+++ Classes/FF_White_Mage.js
@@ -0,0 +1,12 @@
+var iFileName = "Homebrew Syntax - ClassList.js";
+var className = "White Mage";
+RequiredSheetVersion("13.0.6");
+
+// Define the source
+SourceList["FF:A"] = {
+	name: "FFXIV x D&D Compendium: "+ className,
+	abbreviation: "FF:WM",
+	group: "Final Fantasy",
+	url: "https://www.gmbinder.com/share/-LsDqsNbupzeLhkTIcPv",
+	date: "2020/11/25"
+};
