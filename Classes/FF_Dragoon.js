488e02408d21c5338d8028bfaa6b85ea959f0863
lortizg <laura.og179@gmail.com>
2024-08-03 16:05:20 +0200

    Added main features for dragoon

--- Classes/FF_Dragoon.js
+++ Classes/FF_Dragoon.js
@@ -116,7 +116,7 @@ ClassList[className] = {
 			description: "",
 			usages: "Wis mod + Proficiency bonus per ",
 			usagescalc: "event.value = Number(What('Wis Mod') + What('Proficiency Bonus'))",
-			recovery: "ESP"
+			recovery: "Dragon Trance"
 		},
 		jump: {
 			name: "Jump",
@@ -143,14 +143,182 @@ ClassList[className] = {
 				selectNow: true
 			},
 		},
-
-		// dual_cast: {
-		// 	name: "Dual Cast",
-		// 	source: ["FF", 112],
-		// 	minlevel: 1,
-		// 	description: tabbedLine + "I can spend 1 Flair Point to change the casting time from action to bonus for a spell lvl 1-4",
-		// 	action: ["bonus action", "Dual Cast"]
-		// },
+		fighting_style: {
+			name: "Fighting Style",
+			source: ["FF", 65],
+			minlevel: 2,
+			description: tabbedLine + "Choose a Fighting Style using the \"Choose Feature\" button above",
+			choices: ["Archery", "Defense", "Dueling", "Great Weapon Fighting", "Protection", "Two-Weapon Fighting"],
+			choicesNotInMenu: false, //check!! todo
+			archery: {
+				name: "Archery Fighting Style",
+				description: desc("+2 bonus to attack rolls I make with ranged weapons"),
+				calcChanges: {
+					atkCalc: [
+						function (fields, v, output) {
+							if (v.isRangedWeapon && !v.isNaturalWeapon && !v.isDC) output.extraHit += 2;
+						},
+						"My ranged weapons get a +2 bonus on the To Hit."
+					]
+				}
+			},
+			defense: {
+				name: "Defense Fighting Style", //required;
+				description: tabbedLine + "While you are wearing armor, you gain +1 bonus to AC",
+				extraAC: {
+					name: "Defense Fighting Style", // necessary for features referring to fighting style properties directly
+					mod: 1,
+					text: "I gain a +1 bonus to AC while wearing armor.",
+					stopeval: function (v) { return !v.wearingArmor; }
+				}
+			},
+			dueling: {
+				name: "Dueling Fighting Style",
+				description: tabbedLine + "+2 to damage rolls when wielding a melee weapon in one hand and no other weapons",
+				calcChanges: {
+					atkCalc: [
+						function (fields, v, output) {
+							for (var i = 1; i <= FieldNumbers.actions; i++) {
+								if ((/off.hand.attack/i).test(What('Bonus Action ' + i))) return;
+							};
+							if (v.isMeleeWeapon && !v.isNaturalWeapon && !(/((^|[^+-]\b)2|\btwo).?hand(ed)?s?\b/i).test(fields.Description)) output.extraDmg += 2;
+						},
+						"When I'm wielding a melee weapon in one hand and no weapon in my other hand, I do +2 damage with that melee weapon. This condition will always be false if the bonus action 'Off-hand Attack' exists."
+					]
+				}
+			},
+			"great weapon fighting": {
+				name: "Great Weapon Fighting Style",
+				description: tabbedLine + "Reroll 1 or 2 on damage if wielding two-handed/versatile melee weapon in both hands",
+				calcChanges: {
+					atkAdd: [
+						function (fields, v) {
+							if (v.isMeleeWeapon && (/(\bversatile|((^|[^+-]\b)2|\btwo).?hand(ed)?s?)\b/i).test(fields.Description)) {
+								fields.Description += (fields.Description ? '; ' : '') + 'Re-roll 1 or 2 on damage die' + ((/versatile/i).test(fields.Description) ? ' when two-handed' : '');
+							}
+						},
+						"While wielding a two-handed or versatile melee weapon in two hands, I can re-roll a 1 or 2 on any damage die once."
+					]
+				}
+			},
+			protection: {
+				name: "Protection Fighting Style",
+				description: tabbedLine + "As a reaction, I can give disadv. on an attack made vs. someone within 5 ft of me\nI need to be wielding a shield and be able to see the attacker to do this",
+				action: ["reaction", ""]
+			},
+			"Two-Weapon Fighting": {
+				name: "Two-Weapon Fighting Style",
+				description: tabbedLine + "I can add my ability modifier to the damage of my off-hand attacks",
+				calcChanges: {
+					atkCalc: [
+						function (fields, v, output) {
+							if (v.isOffHand) output.modToDmg = true;
+						},
+						"When engaging in two-weapon fighting, I can add my ability modifier to the damage of my off-hand attacks. If a melee weapon includes 'off-hand' or 'secondary' in its name or description, it is considered an off-hand attack."
+					]
+				}
+			},
+		},
+		lancet: {
+			name: "Lancet",
+			source: ["FF", 65],
+			minlevel: 2,
+			description: tabbedLine + "[1 Draconic Blood point] After hitting a melee weapon attack, I recover HP equal to half inflicted dmg (rounded up)."
+		},
+		stall: {
+			name: "Stall",
+			source: ["FF", 65],
+			minlevel: 6,
+			description:
+				tabbedLine + "[Dragon Trance] When I jump, I can pause in place until the end of my next turn. I can do actions as normal in this state." +
+				tabbedLine + "I must use my movement in the next turn to fall."
+		},
+		true_thrust: {
+			name: "True Thrust",
+			source: ["FF", 66],
+			minlevel: 6,
+			description: tabbedLine + "[1 Draconic Blood point] I can my Wis mod to my attack rolls until the end of my next turn.",
+			action: ["bonus action", "True Thrust"]
+		},
+		// heavy_thrust: {
+		// 	name: "Heavy Thrust",
+		// 	source: ["FF", 65],
+		// 	minlevel: 7,
+		// 	description: tabbedLine + "[1 Draconic Blood point] After hitting a melee weapon attack, I recover HP equal to half inflicted dmg (rounded up)."
+		// }
+		life_of_the_dragon: {
+			name: "Life of the Dragon",
+			source: ["FF", 66],
+			minlevel: 9,
+			description: 
+				tabbedLine + "I can now permanently jump 30ft and 15ft high" +
+				tabbedLine + "While on Dragoon Trance it turns into 90ft and 60ft high and I can use Dash as bonus action."
+				//TODO: change abilities of jump on main sheet
+		},
+		high_jump: {
+			name: "High Jump",
+			source: ["FF", 65],
+			minlevel: 10,
+			description: tabbedLine + "If I used Stall in my last turn, if I jump 30ft I can make a melee attack as an action. The creature must succeed a Dex saving throw (disadv.) or be knocked prone and take dmg 4 x Total Damage of the weapon I am wielding; half dmg if succeed and is not knocked.",
+			weaponOptions: {
+				regExpSearch: /^(?=.*high)(?=.*jump).*$/i,
+				name: "High Jump",
+				source: [["FF", 65]],
+				list: "melee",
+				ability: 2,
+				type: "Natural",
+				damage: [0, 0, ""],
+				range: "melee",
+				description: "Dex save (disadv.), success - half dmg, not knocked; I land in an unoccupied space next to the creature",
+				abilitytodamage: false,
+				dc: true,
+				tooltip: "I make 4x full damage of my weapon",
+				isNotWeapon: true,
+				selectNow: true
+			},
+		},
+		wing_clipper: {
+			name: "Wing Clipper",
+			source: ["FF", 66],
+			minlevel: 10,
+			description: tabbedLine + "If a creature fails the DC against my jumps and have flying speed, its flying speed drops to 0. As an action, they can repeat the saving throw and restore it on a success."
+		},
+		fang_and_claw: {
+			name: "Fang and Claw",
+			source: ["FF", 66],
+			minlevel: 13,
+			description: tabbedLine + "[Dragon's Trance] I can make a weapon attack in a bonus action.",
+			action: ["bonus action", "Fang and Claw"]
+		},
+		heavy_impact: {
+			name: "Heavy Impact",
+			source: ["FF", 66],
+			minlevel: 14,
+			description: tabbedLine + "[1 Draconic Blood point] I can make a jump attack affect all crea. within 10ft of the attacked creature."
+		},
+		invigorate: {
+			name: "Invigorate",
+			source: ["FF", 66],
+			minlevel: 17,
+			description: tabbedLine + "If I miss a saving throw, I can reroll the dice and take the new result.",
+			usages: "Wisdom mod per ",
+			usagescalc: "event.value = Number(What('Wis Mod'))",
+			recovery: "long rest"
+		},
+		battle_litany: {
+			name: "Battle Litany",
+			source: ["FF", 66],
+			minlevel: 18,
+			description: 
+				tabbedLine + "[Dragoon's Trance] Me and my allies within 10ft can land a crit on melee weapon attacks on a 19 or 20 roll for STR mod rounds."+
+				tabbedLine + "This can be activated once per Dragoon's Trance. It doesn't overwrite other crit range increases."
+		},
+		dragon_blooded: {
+			name: "Dragon Blooded",
+			source: ["FF", 66],
+			minlevel: 20,
+			description: tabbedLine + "When I roll initiative, I recover 2 uses of Dragoon Trance if I don't have any left."
+		}
 	}
 };
 
